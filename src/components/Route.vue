ffd8c72f4f51f720b39996a82e90b5972056e611
Tsubakura <mingcheemalcolmlok_1994_3d@hotmail.com>
2017-11-19 23:45:37 +0100

    Updating edges for E-4

--- src/components/Route.vue
+++ src/components/Route.vue
@@ -19,7 +19,12 @@ export default {
             if(this.map in this.edges){
                 returnStr = String(this.edges[this.map][route[0]].join("-") + "-");
                 for(let i = 1; i < route.length; i++){
-                    returnStr += String(this.edges[this.map][route[i]][1]+"-");
+                    if(this.edges[this.map].hasOwnProperty(String(route[i]))){
+                        returnStr += String(this.edges[this.map][route[i]][1]+"-");
+                    }
+                    else{
+                        returnStr += String(route[i] +"-");
+                    }
                 }
                 returnStr = returnStr.slice(0,-1);
             }
