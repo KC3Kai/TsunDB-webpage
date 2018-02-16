a10411a6a724f6f519fe447312c4711e51f61427
Tsubakura <mingcheemalcolmlok_1994_3d@hotmail.com>
2018-02-16 20:40:00 +0100

    New difficulty added

--- src/components/Difficulty.vue
+++ src/components/Difficulty.vue
@@ -10,12 +10,15 @@ export default {
             let returnStr = "None";
             switch(diff){
                 case 1:
-                    returnStr = "丙";
+                    returnStr = "丁";
                     break;
                 case 2:
-                    returnStr = "乙";
+                    returnStr = "丙";
                     break;
                 case 3:
+                    returnStr = "乙";
+                    break;
+                case 4:
                     returnStr = "甲";
                     break;
             }
