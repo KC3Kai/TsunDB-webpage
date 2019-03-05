75e7a18c5fdd21aebd96903deef1fb67731c66f8
Tsubakura <mingcheemalcolmlok_1994_3d@hotmail.com>
2019-03-05 18:14:46 +0100

    Adding fluff

--- src/components/Dawn.vue
+++ src/components/Dawn.vue
@@ -7,16 +7,46 @@
         Welcome to my hidden cottage, enjoy your stay!
     </p>
     <div class="content">
-        <a href="https://twitter.com/otk_do/status/988573542529691648" rel="noopener noreferrer" target="_blank">
-            <img src="./../../assets/fluff/kasumi dawn.jpg">
+        <a :href="getSourceLink(id)" rel="noopener noreferrer" target="_blank">
+            <img :src="getImage(id)">
         </a>
+    </div>
+    <div class="content">
+        <strong>Made by <a :href="getAuthorLink(id)" rel="noopener noreferrer" target="_blank">{{getAuthor(id)}}</a></strong>
     </div>
 </div>
 </template>
 
 <script>
 export default {
-    
+    data: function(){
+        return{
+            dawnFluffData: require('./../data/dawnFluff.json'),
+            id: 0
+        }
+    },
+    mounted: function() {
+        this.$nextTick(function () {
+            this.generateRandomFluff();
+        })
+    },
+    methods:{
+        generateRandomFluff(){
+            this.id = Math.floor(Math.random() * this.dawnFluffData.length);
+        },
+        getSourceLink(id){
+            return this.dawnFluffData[this.id].href;
+        },
+        getImage(id){
+            return require(`./../../assets/fluff/dawn/${this.dawnFluffData[this.id].src}`)
+        },
+        getAuthor(id){
+            return this.dawnFluffData[this.id].author;
+        },
+        getAuthorLink(id){
+            return this.dawnFluffData[this.id].authorhome;
+        }
+    }
 }
 </script>
 
--- src/components/Dusk.vue
+++ src/components/Dusk.vue
@@ -7,16 +7,46 @@
         Resting after a long day of hard work...
     </p>
     <div class="content">
-        <a href="https://www.pixiv.net/member.php?id=9209813" rel="noopener noreferrer" target="_blank">
-            <img src="./../../assets/fluff/kasumi and kiyoshimo relaxing.jpg">
+        <a :href="getSourceLink(id)" rel="noopener noreferrer" target="_blank">
+            <img :src="getImage(id)">
         </a>
+    </div>
+    <div class="content">
+        <strong>Made by <a :href="getAuthorLink(id)" rel="noopener noreferrer" target="_blank">{{getAuthor(id)}}</a></strong>
     </div>
 </div>
 </template>
 
 <script>
 export default {
-    
+    data: function(){
+        return{
+            duskFluffData: require('./../data/duskFluff.json'),
+            id: 0
+        }
+    },
+    mounted: function() {
+        this.$nextTick(function () {
+            this.generateRandomFluff();
+        })
+    },
+    methods:{
+        generateRandomFluff(){
+            this.id = Math.floor(Math.random() * this.duskFluffData.length);
+        },
+        getSourceLink(id){
+            return this.duskFluffData[this.id].href;
+        },
+        getImage(id){
+            return require(`./../../assets/fluff/dusk/${this.duskFluffData[this.id].src}`)
+        },
+        getAuthor(id){
+            return this.duskFluffData[this.id].author;
+        },
+        getAuthorLink(id){
+            return this.duskFluffData[this.id].authorhome;
+        }
+    }
 }
 </script>
 
